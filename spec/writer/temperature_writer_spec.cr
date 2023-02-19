db53f1cba3a87df459632efe315808009d821f4c
Aleksander Kwiatkowski <bobikx@poczta.fm>
2023-02-19 20:40:58 +0100

    add temperature writer per month

--- spec/writer/temperature_writer_spec.cr
+++ spec/writer/temperature_writer_spec.cr
@@ -1,7 +1,7 @@
 require "../spec_helper"
 
 describe MeasurementCollector::Writer::TemperatureWriter do
-  it "load CSVs from path" do
+  it "write processed temperature data in single CSV" do
     path = "spec/fixtures/efento_temperature/"
     collector = MeasurementCollector::Collector::EfentoTemperature.new(
       path: path
@@ -14,5 +14,32 @@ describe MeasurementCollector::Writer::TemperatureWriter do
       array: parsed_data
     )
     writer.write
+
+    fixture_path = "spec/fixtures/writers/temperature_writer.csv"
+    fixture_text = File.read(fixture_path)
+    output_text = File.read(output_path)
+    output_text.should eq fixture_text
+  end
+
+  it "write processed temperature data in CSV per month" do
+    path = "spec/fixtures/efento_temperature/"
+    collector = MeasurementCollector::Collector::EfentoTemperature.new(
+      path: path
+    )
+    parsed_data = collector.parse
+
+    # let's use the same path to check if last part is ignored
+    output_path = "temp/temperature_output.csv"
+    writer = MeasurementCollector::Writer::TemperatureWriter.new(
+      path: output_path,
+      array: parsed_data
+    )
+    writer.write_per_month
+
+    fixture_path = "spec/fixtures/writers/temperature_writer.csv"
+    output_month_path = "temp/2022-11.csv"
+    fixture_text = File.read(fixture_path)
+    output_month_text = File.read(output_month_path)
+    output_month_text.should eq fixture_text
   end
 end
--- src/measurement_collector/writer/temperature_writer.cr
+++ src/measurement_collector/writer/temperature_writer.cr
@@ -7,6 +7,10 @@ class MeasurementCollector::Writer::TemperatureWriter
 
   getter :array
 
+  def sorted_array
+    return @array.sort.uniq
+  end
+
   def write
     File.open(@path, "w") do |file|
       file.puts "time;temperature"
@@ -15,10 +19,38 @@ class MeasurementCollector::Writer::TemperatureWriter
         separator: ';',
         quote_char: '"'
       ) do |csv|
-        @array.each do |temperature|
+        sorted_array.each do |temperature|
           csv.row temperature.time.to_unix, temperature.temperature
         end
       end
+    end
+  end
+
+  def write_per_month
+    # no data -> do nothing
+    return if sorted_array.size == 0
+
+    base_path = File.dirname(@path)
+
+    time_from = sorted_array.first.time
+    time_to = sorted_array.last.time
+    time = time_from
+
+    while time <= time_to
+      month_filename = time.to_s("%Y-%m.csv")
+      path_month = File.join([base_path, month_filename])
+
+      month_array = sorted_array.select do |temperature|
+        temperature.time.year == time.year && temperature.time.month == time.month
+      end
+
+      writer = MeasurementCollector::Writer::TemperatureWriter.new(
+        path: path_month,
+        array: month_array
+      )
+      writer.write
+
+      time = time.shift(months: 1)
     end
   end
 end
